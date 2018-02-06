b8d82eeadcfce40b1687502802b2da183ae31823
jenniferlynjohnson <jenniferlynjohnson2000@gmail.com>
2018-02-06 14:01:26 -0800

    Update custom.rb

--- custom.rb
+++ custom.rb
@@ -1,31 +1,26 @@
-use_bpm 130
-
-play:b4
-sleep 0.75
-play:a4
-sleep 0.75
-play:g4
-sleep 0.75
-play:a4
-sleep 0.75
-
-play:b4
-sleep 0.75
-play:b4
-sleep 0.75
-play:b4
-sleep 1.5
-
-play:a4
-sleep 0.75
-play:a4
-sleep 0.75
-play:a4
-sleep 1.5
-
-play:b4
-sleep 0.75
-play:b4
-sleep 0.75
-play:b5
-sleep 1.5
+live_loop :drums do
+  sample :drum_heavy_kick
+  sleep 1
+  use_bpm 100
+  
+  live_loop :drums do
+    sample :drum_heavy_kick
+    sleep 0.5
+    sample :drum_snare_hard
+    sleep 0.5
+    sample :drum_heavy_kick
+    sleep 0.5
+    sample :drum_snare_hard
+    sleep 0.5
+  end
+  live_loop :bass do
+    play :c2
+    sleep 0.25
+    play :c2
+    sleep 2
+    play :e2
+    sleep 0.75
+    play :f2
+    sleep 1
+  end
+end
