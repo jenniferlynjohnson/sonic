d14163352488954b0adf67883d8512cab3b40195
jenniferlynjohnson <jenniferlynjohnson2000@gmail.com>
2018-02-06 14:00:38 -0800

    Create custom.rb

---
+++ custom.rb
@@ -0,0 +1,31 @@
+use_bpm 130
+
+play:b4
+sleep 0.75
+play:a4
+sleep 0.75
+play:g4
+sleep 0.75
+play:a4
+sleep 0.75
+
+play:b4
+sleep 0.75
+play:b4
+sleep 0.75
+play:b4
+sleep 1.5
+
+play:a4
+sleep 0.75
+play:a4
+sleep 0.75
+play:a4
+sleep 1.5
+
+play:b4
+sleep 0.75
+play:b4
+sleep 0.75
+play:b5
+sleep 1.5
