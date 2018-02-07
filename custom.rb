2f96884954404099781dea82e473436bea87d1ca
jenniferlynjohnson <jenniferlynjohnson2000@gmail.com>
2018-02-07 14:19:44 -0800

    Update custom.rb

--- custom.rb
+++ custom.rb
@@ -1,26 +1,57 @@
-live_loop :drums do
-  sample :drum_heavy_kick
-  sleep 1
-  use_bpm 100
-  
-  live_loop :drums do
-    sample :drum_heavy_kick
-    sleep 0.5
-    sample :drum_snare_hard
-    sleep 0.5
-    sample :drum_heavy_kick
-    sleep 0.5
-    sample :drum_snare_hard
-    sleep 0.5
-  end
-  live_loop :bass do
-    play :c2
-    sleep 0.25
-    play :c2
-    sleep 2
-    play :e2
-    sleep 0.75
-    play :f2
-    sleep 1
-  end
-end
+
+use_synth :piano
+play :e3
+sleep 0.5
+play :d2
+sleep 0.5
+play :c1
+sleep 0.5
+play :d2
+sleep 0.5
+play :e3
+sleep 0.5
+play :e3
+sleep 0.5
+play :e3
+sleep 1
+
+play :d2
+sleep 0.5
+play :d2
+sleep 0.5
+play :d2
+sleep 1
+
+play :e3
+sleep 0.5
+play :g5
+sleep 0.5
+play :g5
+sleep 1
+
+play :e3
+sleep 0.5
+play :d2
+sleep 0.5
+play :c1
+sleep 0.5
+play :d2
+sleep 0.5
+play :e3
+sleep 0.5
+play :e3
+sleep 0.5
+play :e3
+sleep 0.5
+
+play :e3
+sleep 0.5
+play :d2
+sleep 0.5
+play :d2
+sleep 0.5
+play :e3
+sleep 0.5
+play :d2
+sleep 0.5
+play :c1
